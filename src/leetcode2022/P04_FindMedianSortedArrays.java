c4b6a0c29bed0aaae87700e219ad4bc6beebf924
Qiuzhiyuan <845026103@qq.com>
2022-08-10 16:00:07 +0800

    Find median sorted arrays

---
+++ src/leetcode2022/P04_FindMedianSortedArrays.java
@@ -0,0 +1,102 @@
+package leetcode2022;
+
+import common.Tools;
+
+/*
+Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+
+The overall run time complexity should be O(log (m+n)).
+
+
+
+Example 1:
+
+Input: nums1 = [1,3], nums2 = [2]
+Output: 2.00000
+Explanation: merged array = [1,2,3] and median is 2.
+
+Example 2:
+
+Input: nums1 = [1,2], nums2 = [3,4]
+Output: 2.50000
+Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
+
+
+
+Constraints:
+
+    nums1.length == m
+    nums2.length == n
+    0 <= m <= 1000
+    0 <= n <= 1000
+    1 <= m + n <= 2000
+    -10^6 <= nums1[i], nums2[i] <= 10^6
+
+
+ */
+
+// Un submit
+
+public class P04_FindMedianSortedArrays {
+
+    public static void main(String args[]) {
+        P04_FindMedianSortedArrays test = new P04_FindMedianSortedArrays();
+        Tools.println(test.findMedianSortedArrays(new int[]{1}, new int[]{3}));
+        Tools.println(test.findMedianSortedArrays(new int[]{1}, new int[]{}));
+        Tools.println(test.findMedianSortedArrays(new int[]{}, new int[]{3}));
+        Tools.println(test.findMedianSortedArrays(new int[]{}, new int[]{3, 4}));
+        Tools.println(test.findMedianSortedArrays(new int[]{3, 4}, new int[]{}));
+        Tools.println(test.findMedianSortedArrays(new int[]{3, 4, 5}, new int[]{}));
+        Tools.println(test.findMedianSortedArrays(new int[]{}, new int[]{3, 4, 5}));
+        Tools.println(test.findMedianSortedArrays(new int[]{1, 2}, new int[]{3}));
+        Tools.println(test.findMedianSortedArrays(new int[]{1}, new int[]{0, 3}));
+        Tools.println(test.findMedianSortedArrays(new int[]{1, 2, 7, 9}, new int[]{3, 4, 8}));
+        Tools.println(test.findMedianSortedArrays(new int[]{1, 2, 10, 10}, new int[]{0, 3, 7, 9, 10}));
+        Tools.println(test.findMedianSortedArrays(new int[]{5, 5, 5}, new int[]{4, 4, 4}));
+        Tools.println(test.findMedianSortedArrays(new int[]{5, 5, 5, 5}, new int[]{4, 4, 4}));
+        Tools.println(test.findMedianSortedArrays(new int[]{5, 5}, new int[]{4, 4, 4}));
+        Tools.println(test.findMedianSortedArrays(new int[]{1, 3, 5, 7, 9}, new int[]{2, 4, 6, 8}));
+        Tools.println(test.findMedianSortedArrays(new int[]{1, 3, 3, 5, 6, 7, 7}, new int[]{2, 3, 4, 5, 6, 7, 9}));
+        Tools.println(test.findMedianSortedArrays(new int[]{1, 3, 3, 5}, new int[]{5, 5, 5}));
+        Tools.println(test.findMedianSortedArrays(new int[]{1, 2, 3, 4}, new int[]{}));
+        Tools.println(test.findMedianSortedArrays(new int[]{}, new int[]{1, 2, 3, 4}));
+    }
+
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        final boolean isOdd = (nums1.length + nums2.length) % 2 == 1;
+        int m = 0, n = 0;
+        if (isOdd) {
+            int p = 0;
+            int mid = (nums1.length + nums2.length) / 2 + 1;
+            while (m + n < mid) {
+                while (m < nums1.length && m + n < mid && (n >= nums2.length || nums1[m] <= nums2[n])) {
+                    p = nums1[m];
+                    m++;
+                }
+                while (n < nums2.length && m + n < mid && (m >= nums1.length || nums2[n] <= nums1[m])) {
+                    p = nums2[n];
+                    n++;
+                }
+            }
+            return p;
+        } else {
+            int mid = (nums1.length + nums2.length) / 2 - 1;
+            while (m + n < mid) {
+                while (m < nums1.length && m + n < mid && (n >= nums2.length || nums1[m] <= nums2[n])) {
+                    m++;
+                }
+                while (n < nums2.length && m + n < mid && (m >= nums1.length || nums2[n] <= nums1[m])) {
+                    n++;
+                }
+            }
+            if (m < nums1.length && n < nums2.length) {
+                return (nums1[m] + nums2[n]) / 2d;
+            } else if (m < nums1.length - 1) {
+                return (nums1[m] + nums1[m + 1]) / 2d;
+            } else if (n < nums2.length - 1) {
+                return (nums2[n] + nums2[n + 1]) / 2d;
+            }
+        }
+        return 0;
+    }
+}
