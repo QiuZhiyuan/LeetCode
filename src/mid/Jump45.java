36b4b23ba45ee26555a1d9c73889433f30db7ce4
Qiuzhiyuan <845026103@qq.com>
2024-12-09 12:01:08 +0800

    45 换了种无聊的解法

--- src/mid/Jump45.java
+++ src/mid/Jump45.java
@@ -9,28 +9,28 @@ public class Jump45 {
         System.out.println(new Jump45().jump(new int[]{2, 3, 0, 1, 4}));// 2
         System.out.println(new Jump45().jump(new int[]{2, 0, 2, 0, 4}));// 2
         System.out.println(new Jump45().jump(new int[]{1, 3, 0, 1, 0}));// 2
-        System.out.println(new Jump45().jump(new int[]{9}));// 2
+        System.out.println(new Jump45().jump(new int[]{9}));// 0
         System.out.println(new Jump45().jump(new int[]{2, 2, 0, 1, 2, 0, 1, 9}));// 5
+        System.out.println(new Jump45().jump(new int[]{1, 1, 1, 1, 1, 1, 1}));// 6
     }
 
     public int jump(int[] nums) {
-        int[] count = new int[nums.length];
-        count[count.length - 1] = 0;
+        int flag = 100000;
         for (int i = nums.length - 2; i >= 0; i--) {
-            int min = Integer.MAX_VALUE;
-            count[i] = min - 1;
-            for (int j = nums[i]; j > 0; j--) {
+            int min = 10000;
+            nums[i] = min * flag + nums[i];
+            for (int j = nums[i] % flag; j > 0; j--) {
                 int p = i + j;
-                if (p < count.length) {
-                    if (min > count[p] + 1) {
-                        min = count[p] + 1;
-                        count[i] = min;
+                if (p < nums.length) {
+                    if (min > nums[p] / flag + 1) {
+                        min = nums[p] / flag + 1;
+                        nums[i] = min * flag + nums[i] % flag;
                     }
                 } else {
-                    count[i] = 1;
+                    nums[i] = flag + nums[i] % flag;
                 }
             }
         }
-        return count[0];
+        return nums[0] / flag;
     }
 }
